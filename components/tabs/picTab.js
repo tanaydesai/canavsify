67921c9c0fe0063b6666ec4d79a6af9b842e10fc
TanayDesai <tanaydesai5.com>
2023-06-16 03:38:21 +0530

    V0.0.6

--- components/tabs/picTab.js
+++ components/tabs/picTab.js
@@ -6,7 +6,7 @@ import {motion} from 'framer-motion'
 const PicTab = ({pic,link,width,height,drag,x,y}) => {
     return (        
         <motion.div whileHover={{scale:1.04,transition:{duration:0.5}}} transition={{duration:0.5}} drag={drag} dragMomentum={false}  className={styles.picDisplay} style={{x:x,y:y}}>
-            <Image onClick={() => window.open(link)} style={{borderRadius:"12px",margin:"auto",pointerEvents:"none"}} src={pic} width={width} height={height}></Image>
+            <Image onClick={() => window.open(link)} style={{borderRadius:"12px",margin:"auto"}} src={pic} width={width} height={height}></Image>
         </motion.div>
     )
 }
